15b0483ca8dc6c14d87b66e182d82848692dfdeb
Dounx <imdounx@gmail.com>
2018-03-28 12:34:28 +0800

    Fix some bugs

--- app/src/main/java/Util/DateFormatter.java
+++ app/src/main/java/Util/DateFormatter.java
@@ -36,7 +36,7 @@ public class DateFormatter {
     public static String ParseDateToString(Date date, Locale locale, Context context) {
         SimpleDateFormat dateFormat;
         if (UserPreferences.getStoredLanguage(context).equals("Chinese")) {
-            dateFormat = new SimpleDateFormat("yyyy MMM d", locale);
+            dateFormat = new SimpleDateFormat("yyyy mmm d", locale);
         } else {
             dateFormat = new SimpleDateFormat("MMM d, yyyy", locale);
         }
